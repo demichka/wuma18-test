8e9fc1cd0f8fbb901e9531aa1f9d62ecff384bae
Zhenya <demichka@gmail.com>
2018-10-09 20:36:55 +0200

    added 5, 6 exercises

--- www/js/main.js
+++ www/js/main.js
@@ -6,3 +6,35 @@ function printName(name) {
 }
 
 console.log('2: ' + printName('Zhenya'));
+
+function answerName() {
+    let res = prompt('Vad heter du?', 'Name');
+        return res;
+}
+
+
+let userName = answerName();
+
+if (userName === null || userName === '') {
+    console.log('4: Good bye!');
+}
+else {
+    console.log('3: ' + printName(userName));
+}
+
+function logNumbers() {
+    console.log('5 exercise: ');
+    for(let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+}
+logNumbers();
+
+function logNumbers2() {
+    console.log('6 exercise: ');
+    for(let i = 5; i < 16; i++) {
+        console.log(i);
+    }
+}
+
+logNumbers2();
