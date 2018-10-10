a7b5d43c86599d9a95e415c6db45ca06dc990517
Zhenya <demichka@gmail.com>
2018-10-10 10:51:06 +0200

    added 10 exercise

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,7 @@
 console.log('1: ' + 'Hello world!');
 
 function printName(name) {
-    let res =  'Hello, ' + name + '!';
+    let res = 'Hello, ' + name + '!';
     return res;
 }
 
@@ -9,7 +9,7 @@ console.log('2: ' + printName('Zhenya'));
 
 function answerName() {
     let res = prompt('Vad heter du?', 'Name');
-        return res;
+    return res;
 }
 
 
@@ -17,14 +17,13 @@ let userName = answerName();
 
 if (userName === null || userName === '') {
     console.log('4: Good bye!');
-}
-else {
+} else {
     console.log('3: ' + printName(userName));
 }
 
 function logNumbers() {
     console.log('5 exercise: ');
-    for(let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
         console.log(i);
     }
 }
@@ -32,7 +31,7 @@ logNumbers();
 
 function logNumbers2() {
     console.log('6 exercise: ');
-    for(let i = 5; i < 16; i++) {
+    for (let i = 5; i < 16; i++) {
         console.log(i);
     }
 }
@@ -41,7 +40,7 @@ logNumbers2();
 
 function logNumbers3() {
     console.log('7 exercise: ');
-    for(let i = 10; i >= 1; i--) {
+    for (let i = 10; i >= 1; i--) {
         console.log(i + '...');
     }
 }
@@ -51,7 +50,7 @@ logNumbers3();
 function logNumbers4() {
     console.log('8 exercise: ');
     let string = '';
-    for(let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 10; i++) {
         string += i + '...';
     }
     console.log(string);
@@ -59,10 +58,26 @@ function logNumbers4() {
 
 logNumbers4();
 
-
 function isEven(aNumber) {
     let res = aNumber % 2;
     return res;
 }
 
 console.log('9 exercise: ' + isEven(4));
+
+function logNumbers5() {
+    let string = '';
+    for (let i = 1; i <= 10; i++) {
+        isEven(i);
+        if (!isEven(i)) {
+            string += i + '...';
+        } else {
+            string += i + '.';
+        }
+    }
+    return string;
+}
+
+
+
+console.log('10 exercise: ' + logNumbers5());
